ef84d9340ccbbcc6e7dffcdafa52239a787d663e
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-08-11 15:48:22 +0200

    Initial commit

---
+++ src/main/java/com/lechowicz/footballmatchesresults/FootballMatchesResultsApplication.java
@@ -0,0 +1,13 @@
+package com.lechowicz.footballmatchesresults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMatchesResultsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballMatchesResultsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lechowicz/footballmatchesresults/FootballMatchesResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lechowicz.footballmatchesresults;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMatchesResultsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
